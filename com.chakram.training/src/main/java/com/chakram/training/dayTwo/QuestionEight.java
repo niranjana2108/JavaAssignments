d1b875bff56caff8c3548f75503a35a40040a17e
sri865 <sri86.ece@gmail.com>
2023-12-19 09:42:43 +0530

    test

--- com.chakram.training/src/main/java/com/chakram/training/dayTwo/QuestionEight.java
+++ com.chakram.training/src/main/java/com/chakram/training/dayTwo/QuestionEight.java
@@ -6,7 +6,7 @@ public class QuestionEight {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi1");
+System.out.println("hi1");//this is test
 	}
 
 }
